24eb4f3207da11cc60ebeef5bde141467adcad5f
pawan07thapa <[sajan.thapa12345@gmail.com]>
2019-06-16 00:57:47 +0530

    adding my projects

---
+++ FirstProject/src/main/java/com/pawan/demo/FirstProjectApplication.java
@@ -0,0 +1,18 @@
+package com.pawan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class FirstProjectApplication {
+
+	public static void main(String[] args) {
+	ConfigurableApplicationContext context=	SpringApplication.run(FirstProjectApplication.class, args);
+	
+	Alien a= context.getBean(Alien.class);
+	System.out.println(a);
+	
+	}
+
+}
